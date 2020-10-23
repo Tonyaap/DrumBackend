a4df89b27e3783dbedbc58e3a6e5bd3aa0da44f1
Stefan <69578301+Tonyaap@users.noreply.github.com>
2020-10-23 11:31:20 +0200

    Update config.js
    Added the url for db

--- config/config.js
+++ config/config.js
@@ -12,11 +12,9 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
+  
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    url: process.env.DB_URL || "localhost",
+    dialect: "postgres",
   },
 };
